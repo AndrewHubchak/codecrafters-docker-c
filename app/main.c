91e1e9961b3409e2fa2cbf8aaeb3a01f2553369a
Andrew Hubchak <andrew.hubchak@gmail.com>
2024-09-12 08:06:06 +0300

    sandbox

--- app/main.c
+++ app/main.c
@@ -3,8 +3,47 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+const char* tmpDirectory = "./sandbox";
+const char* sandboxCommand = "./sandbox/ex";
+
+bool createDir(const char* dirName) {
+	struct stat st = {0};
+	bool result = false;
+
+	if (stat(dirName, &st) == -1) {
+		mkdir(dirName, 0700);
+		result = true;
+	}
+	return result;
+}
+
+void copy_file(const char *from, const char *to) {
+	struct stat file_stats;
+	int fd_from = open(from, O_RDONLY);
+
+	fstat(fd_from, &file_stats);
+
+	void *address = mmap(0, file_stats.st_size, PROT_READ, MAP_PRIVATE, fd_from, 0);
+
+	int fd_to = creat(to, 0777);
+
+	write(fd_to, address, file_stats.st_size);
+
+	close(fd_from);
+
+	close(fd_to);
+
+}
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
+	int status = EXIT_FAILURE;
 	// Disable output buffering
 	setbuf(stdout, NULL);
 
@@ -13,21 +52,22 @@ int main(int argc, char *argv[]) {
 
 	if (pipe(pipefd) == -1) {
 		printf("was not able to create pipe");
-		exit(EXIT_FAILURE);
+		exit(status);
 	}
 
 	if (pipe(pipefderr) == -1) {
 		printf("was not able to create pipe for stderr");
-		exit(EXIT_FAILURE);
+		exit(status);
 	}
 
-	char *command = argv[3];
+	const char *command = argv[3];
+
+	createDir(tmpDirectory);
+
+	copy_file(command, sandboxCommand);
+	chroot(tmpDirectory);
+
 	int child_pid = fork();
-	if (child_pid == -1) {
-	    printf("Error forking!");
-	    exit(EXIT_FAILURE);
-	}
-
 	if (child_pid == 0) {
 		// Replace current program with calling program
 		close(pipefd[0]);
@@ -44,18 +84,18 @@ int main(int argc, char *argv[]) {
 		close(pipefd[1]);
 		close(pipefderr[1]);
 
-		int exec_status = execv(command, &argv[3]);
-		exit(EXIT_SUCCESS);
-	} else {
+		int exec_status = execv(sandboxCommand, &argv[3]);
+		status = EXIT_SUCCESS;
+	} else if (child_pid > 0) {
 		// We're in parent
 		close(pipefd[1]); // close write end of the pipe
 		close(pipefderr[1]); // close write end of the pipe
 		char buf;
-		while ( read(pipefd[0], &buf, 1) > 0) {
+		while (read(pipefd[0], &buf, 1) > 0) {
 			write(STDOUT_FILENO, &buf, 1);
 		}
 
-		while ( read(pipefderr[0], &buf, 1) > 0) {
+		while (read(pipefderr[0], &buf, 1) > 0) {
 			write(STDERR_FILENO, &buf, 1);
 		}
 		close(pipefd[0]); // close read end of the pipe
@@ -65,14 +105,17 @@ int main(int argc, char *argv[]) {
 		int child_exit_code = 0;
 		int wait_return_val = -1;
 		wait_return_val = waitpid(child_pid, &child_exit_code, 0);
-		if ( wait_return_val == -1) {
+		if (wait_return_val == -1) {
 			exit(EXIT_FAILURE);
 		}
 
 		if (WIFEXITED(child_exit_code)) {
 			exit(WEXITSTATUS(child_exit_code));
 		}
+	} else {
+	    printf("Error forking!");
+	    status = EXIT_FAILURE;
 	}
 
-	exit(EXIT_FAILURE);
+	exit(status);
 }
